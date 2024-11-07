f624feb4cc06881e903bbacbda665b05073d6c67
sajani <it20117696@my.sliit.lk>
2024-11-07 22:35:08 +0530

    auth folder

--- Server/server.js
+++ Server/server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const cors = require("cors");
 require("express-async-errors");
 
-//Database
+//Database connection
 const databaseConfig = require("./database/database.config");
+
 const errorHandlerMiddleware = require("./error/error.middleware");
-const NotFound = require("./error/error.classes/NotFoundError");
+const NotFoundError = require("./error/error.classes/NotFoundError");
 
 const constants = require("./constants");
 require("dotenv").config();
@@ -16,10 +17,15 @@ app.use(cors());
 app.use(express.json());
 
 //import routes
+const AuthRouter = require("./auth/auth.route");
 const RecipeRouter = require("./recipe/recipe.route");
 
 //defines routes
+app.use(constants.API.PREFIX.concat("/auth"), AuthRouter);
 app.use(constants.API.PREFIX.concat("/receipe"), RecipeRouter);
+
+//error handler middleware
+app.use(errorHandlerMiddleware);
 
 //404 not found route
 app.use((req, res, next) => {
